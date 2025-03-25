05c15c65eb4c2b6c707aacd7a682b70ae5ec6897
brian070069 <kkipngeno409@gmail.com>
2025-03-25 17:36:54 +0300

    updated commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,48 +1,9 @@
-public class BankAccount {
+public class BankAccount{
     private String accountNumber;
-
-    private double accountBalance;
     private String customerName;
     private String email;
+    private double accountBalance;
     private String phoneNumber;
-
-    public BankAccount() {
-        this("011087193387000",100,"Josh","josh@gmail.com","+2547022222567");
-        System.out.println("Empty constructor");
-    }
-
-    public BankAccount(String accountNumber,double accountBalance,String customerName,String email,String phoneNumber){
-        System.out.println("BankAccount constructor with parameters called");
-        this.accountNumber=accountNumber;
-        this.accountBalance=accountBalance;
-        this.customerName=customerName;
-        email=email;
-        phoneNumber=phoneNumber;
-    }
-
-    public BankAccount(String customerName, String email, String phoneNumber) {
-        this("011076762",250.50,customerName,email,phoneNumber);
-        this.customerName = customerName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public double depositFunds(double depositAmount) {
-        accountBalance +=depositAmount;
-        System.out.println(customerName + " has deposited Ksh"+ depositAmount + " to Account Number " +accountNumber+".New Bank Balance is Ksh" +accountBalance);
-        return accountBalance;
-    }
-
-    public void withdrawFunds(double withdrawAmount) {
-        if(withdrawAmount>accountBalance) {
-            System.out.println("Insufficient bank balance.Please try a lower amount");
-        } else {
-            accountBalance-= withdrawAmount;
-            System.out.println(customerName+" has withdrawn Ksh"+withdrawAmount + " from Account Number " +accountNumber +".New balance is Ksh"+accountBalance);
-
-        }
-
-    }
 
     public String getAccountNumber() {
         return accountNumber;
@@ -50,14 +11,6 @@ public class BankAccount {
 
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
-    }
-
-    public double getAccountBalance() {
-        return accountBalance;
-    }
-
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
     }
 
     public String getCustomerName() {
@@ -76,11 +29,35 @@ public class BankAccount {
         this.email = email;
     }
 
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        accountBalance += depositAmount;
+        System.out.println("Ksh "+ depositAmount + " deposited to Account "+ accountNumber +"" +
+                ". Your new Account balance is Ksh "+accountBalance);
+    }
+
+    public void withdraw( double withdrawAmount) {
+        if(withdrawAmount > accountBalance) {
+            System.out.println("You have insufficient balance . Please try a lower amount");
+        } else{
+            accountBalance -=withdrawAmount;
+            System.out.println("Ksh "+withdrawAmount + " withdrawn from account "+accountNumber+
+                    ".New account balance is Ksh "+accountBalance);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,22 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        BankAccount kip = new BankAccount();
+        kip.setAccountBalance(200000);
+        kip.setAccountNumber("0110871938700");
+        kip.setEmail("kip@gmail.com");
+        kip.setPhoneNumber("0702922235");
+        kip.setCustomerName("Kipngeno");
+        kip.deposit(50000);
+        kip.withdraw(150000);
+        kip.deposit(35000);
+        kip.withdraw(140000);
+        kip.deposit(200000);
+        kip.withdraw(335000);
+        kip.deposit(100);
+        kip.withdraw(54.55);
+        kip.withdraw(45.46);
+        kip.withdraw(45.45);
 
-       //BankAccount bankAccount= new BankAccount("011087193387",10000,"KIPNGENO","kipngeno@gmail.com","0702922234");
-        BankAccount bankAccount = new BankAccount();
-        System.out.println(bankAccount.getAccountBalance());
-        System.out.println(bankAccount.getPhoneNumber());
-        System.out.println(bankAccount.getAccountNumber());
-//        System.out.println("customer name:"+bankAccount.getCustomerName());
-//        System.out.println("Account Balance:"+bankAccount.getAccountBalance());
-//        System.out.println("Account number:"+bankAccount.getAccountNumber());
-//        System.out.println("Email:"+bankAccount.getEmail());
-//        System.out.println("Phone Number:"+bankAccount.getPhoneNumber());
-
-        BankAccount joya = new BankAccount("Joya","joya@gmail.com","0721987789");
-        System.out.println("customer Name: "+ joya.getCustomerName() + " Account Number:"+joya.getAccountNumber()+ " Account Balance:"+joya.getAccountBalance()+ " Email "+joya.getEmail()+" Phone Number:"+joya.getPhoneNumber());
-
-        bankAccount.withdrawFunds(500);
-        bankAccount.depositFunds(5000);
-        bankAccount.withdrawFunds(2000);
-        bankAccount.withdrawFunds(3000);
-
-        bankAccount.depositFunds(1000);
-        bankAccount.withdrawFunds(450.55);
-
-        bankAccount.withdrawFunds(549.46);
-        bankAccount.withdrawFunds(549.45);
-
+        System.out.println(kip.getAccountBalance());
     }
 }
